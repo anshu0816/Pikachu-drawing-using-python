b48da4544a999ee4d6e5969771b993ed71be3167
Anshu Gaur <anshugaur024@gmail.com>
2025-02-23 13:22:09 +0530

    Add initial files for Pikachu drawing project

---
+++ Gusess_game/app.js
@@ -0,0 +1,29 @@
+// const max = prompt("enter the max number");
+// console.log(max);
+
+// const random = Math.floor(Math.random() * max) + 1;
+// let guess = prompt("guess the number");
+
+// while (true) {
+//     if (guess == "quit") {
+//         console.log("user quit");
+//         break;
+//     }
+//     if (guess == random) {
+//         console.log("You are right! congrats!! random number was", random);
+//         break;
+//     }
+//     else if (guess < random) {
+//         guess = prompt("Your guess was too small. Please try again");
+//     }
+//     else {
+//         guess = prompt("Your guess was too large. Please try again");
+//     }
+// }
+
+
+
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+
---
+++ pattern.java
@@ -0,0 +1,33 @@
+public class pattern {
+    public static void main(String[] args) {
+        //int n = 5; 
+        // for (int i = 1; i <= n; i++) {
+        //     // Print stars in each row
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //   
+        //     System.out.println();
+        // }
+
+
+        //int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n-i+1; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        int n = 6;
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " "); 
+                ch++;
+            }
+            System.out.println();
+        }
+        
+    }
+}
